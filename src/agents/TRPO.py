06b7966317b2a0a16d136a5498539c6ad9090258
ashishsai960 <2021csb1122@iitrpr.ac.in>
2025-03-31 16:11:10 +0530

    Update TRPO.py

--- src/agents/TRPO.py
+++ src/agents/TRPO.py
@@ -1,14 +1,14 @@
 from sb3_contrib import TRPO
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import EvalCallback
 import os
 
 def train_trpo(env_fn, total_timesteps=100000, model_path="trpo_model", eval_env_fn=None):
-    env = make_vec_env(env_fn, n_envs=1)
+    env = DummyVecEnv([env_fn])
 
     eval_callback = None
     if eval_env_fn:
-        eval_env = make_vec_env(eval_env_fn, n_envs=1)
+        eval_env = DummyVecEnv([eval_env_fn])
         eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
                                      log_path="./logs/", eval_freq=10000)
 
